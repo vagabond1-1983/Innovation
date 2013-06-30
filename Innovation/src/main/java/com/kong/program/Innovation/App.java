839b15549afc81d6402ff1eb7ac91f0a632a6ca8
devin kong <devin.kong@gmail.com>
2013-06-30 22:41:32 +0800

    First push. Nothing

---
+++ Innovation/src/main/java/com/kong/program/Innovation/App.java
@@ -0,0 +1,13 @@
+package com.kong.program.Innovation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
